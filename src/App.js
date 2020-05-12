18c39e5b3524edd76556db7a6e8a78f04bb65559
Eric Energy <ericnrgnash@gmail.com>
2020-05-12 16:27:45 -0500

    Link fix

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap'
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Contact from "./components/contact";
 import Main from "./components/main";
 import Portfolio from "./components/portfolio";
-
-import logo from "./logo.png";
+import './fonts/PlayfairDisplay-VariableFont_wght.ttf'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" target="_blank">
-              <img src={logo} width="30" height="30"/>
-            </a>
-            <Link to="/" className="navbar-brand">Eric Simmons</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Portfolio</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Contact Me</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={Main} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/portfolio" component={Portfolio} />
-        </div>
+        <Route exact path="/" component={Main} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/portfolio" component={Portfolio} />
       </Router>
     );
   }
--- src/components/main.js
+++ src/components/main.js
@@ -1,45 +1,118 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap'
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import logo from '../images/underc.png';
+import logo1 from '../images/e3.5.png';
+import logo from '../images/underc.jpg';
+import logoM from '../images/mern.png';
+import resume from "../images/Eric_Simmons_Resume.pdf"
 
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class TodosList extends Component {
     render() {
         return (
             <div>
+                <div clasName="container1">
+                    <div className="parallax1">
+                        <Navbar id="navbarId" fixed="top" className="navbar-collapse" collapseOnSelect expand="lg" variant="dark">
+                            <Navbar.Brand href="/mern-My-Portfolio"><img src={logo1} width="200" height="" /></Navbar.Brand>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav className="mr-auto">
+                                </Nav>
+                                <Nav aria-label="Toggle navigation">
+                                    <Link className="links" to="/portfolio">Portfolio</Link>
+                                    <Link className="links" eventKey={2} to="/contact"> Contact </Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Navbar>
+                        <h1 md="auto" className="text-center" id="frontPage">Eric Simmons</h1>
+                        <h4 className="text-center" id="frontPageLine2">Creative</h4>
+                    </div>
+                </div>
+
+                <img src={logoM} className="mern" height="50" width="200" alt="mernlogo" />
+                <h2 id="name" className="text-center">Welcome! </h2> <h4 className="text-center mb-4"> Full Stack Developers are computer programmers who are proficient in both front and back end coding. Their primary responsibilities include designing user interactions on websites, developing servers and databases for website functionality and coding for mobile platforms</h4>
+                <Jumbotron id="jumbotron">
+
+                    <Row>
+                        <Col md="auto" className="text-center mb-4">
+                            <img src={logo} id="profilePic" height="300" width="300" alt="profilepicture" /><br></br>
+                            <a className="ml-4" href="https://www.linkedin.com/in/ericsimmonsnash/">Linkedin  |</a>
+                            <a href="https://github.com/EricEnergy"> Github</a>
+                            <p className="ml-4"> <a href={resume}>My Resume</a></p>
+                        </Col>
+                        <Col class="container-fluid">
+                            <h2>About me</h2>
+                            <hr></hr>
+                            <h6 className="mb-4">Goal-oriented web developer with strong commitment to collaboration and solutions oriented problem-solving. Utilizes various web design software to develop customer-focused websites and designs. Committed to high standards of web design, user experience and usability for multiple types of end-users. Graphic design background results in judicious, creative, effective websites, apps, and platforms, which propel competitive advantage and revenue growth.
+                            </h6>
+                        </Col>
+                    </Row>
+                </Jumbotron>
+
+                <div clasName="container">
+                    <div className="parallax2">
+                    </div>
+                </div>
+                <Row>
+                    <Col>
+                        <h5 className="text-center mt-4 mb-4"> "Eric and I went through the Vanderbilt Coding Boot Camp together, and throughout the program, Eric made great strides as a web developer. He continuously proved that he was willing to put the time and effort required to accomplish whatever goals he set.
+                                                            As we worked on a group project together, Eric worked tirelessly to do whatever was necessary to create a viable, functioning application, and was consistently available and engaged to help out wherever he was needed. I believe that Eric's level of motivation and determination would make him an asset to any team." </h5>
+                    </Col>
+                </Row>
+
                 <Jumbotron>
                     <Row>
-                        <Col> </Col>
-                        <Col className="text-center powder"><img src={logo} height="200" width="200" margi="auto" alt="Logo" /></Col>
-                        <Col> </Col>
+                        <Col></Col>
+                        <Col><h4 className=" ml-5 mt-4 mb-4" id="dark">
+                            <u>Technologies I've Used</u>
+                        </h4></Col>
+                        <Col></Col>
+                        
                     </Row>
-                    <Row>
-                        <Col className="text-center powder"><h3>Web site under construction.</h3></Col>
-                    </Row>
-                    <Row className="text-center">
-                        <Col> </Col>
-                        <Col md="auto">
-                            <h6>In the mean time feel free to visit my Linkedin where I have featured  <br />projects or my Github where you will see my top starred projects with <br />links to my deployed work.
-                        <br/>
-                        -Eric Simmons
-                             </h6>
-                        </Col>
-                        <Col> </Col>
-                    </Row>
-                    <Row className="text-center">
-                    <Col> </Col>
-                        <Col md="auto">
-                        <a href="https://www.linkedin.com/in/ericsimmonsnash/">Linkedin  |</a>
-                        <a href="https://github.com/EricEnergy"> Github</a>
-                        </Col>
-                        <Col> </Col>
-                    </Row>
-
+                    <h4 id="dark">
+                        <Row>
+                            <Col> <ul>
+                                <li>HTML, CSS</li>
+                                <li>Bootstrap/Foundation </li>
+                                <li>jQuery, AJAX </li>
+                                <li>Jest, Mocha, Chai </li>
+                            </ul>
+                            </Col>
+                            <Col>
+                                <ul>
+                                    <li>JavaScrpit/[ES5,ES6]</li>
+                                    <li>Node.js, NPM</li>
+                                    <li>Express</li>
+                                    <li>Github</li>
+                                </ul>
+                            </Col>
+                            <Col>
+                                <ul>
+                                    <li>MySQL, Sequelize</li>
+                                    <li>React.js</li>
+                                    <li>MongoDB</li>
+                                    <li>Google Suite</li>
+                                </ul>
+                            </Col>
+                            <Col>
+                                <ul>
+                                    <li>Photoshop</li>
+                                    <li>Illustrator</li>
+                                    <li>Visual Studio Code</li>
+                                    <li>Google Suite</li>
+                                </ul>
+                            </Col>
+                        </Row>
+                    </h4>
                 </Jumbotron>
+                <div className="parallax1_5">
+                </div>
             </div>
         )
     }
