98549279384e621bb8603df63499df8e44774dec
Eric Energy <ericnrgnash@gmail.com>
2020-05-28 12:24:52 -0500

    link fix

---
+++ build/precache-manifest.692471614be382d1ded2092c7c85c7ce.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd8cad5a3d8dde87a7567a4e48dd924",
+    "url": "/mern-My-Portfolio/index.html"
+  },
+  {
+    "revision": "8eee0783aaa11fd6e102",
+    "url": "/mern-My-Portfolio/static/css/2.2990c7a7.chunk.css"
+  },
+  {
+    "revision": "aef5ad1c611bbacb11c8",
+    "url": "/mern-My-Portfolio/static/css/main.258d678a.chunk.css"
+  },
+  {
+    "revision": "8eee0783aaa11fd6e102",
+    "url": "/mern-My-Portfolio/static/js/2.ef904470.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/mern-My-Portfolio/static/js/2.ef904470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef5ad1c611bbacb11c8",
+    "url": "/mern-My-Portfolio/static/js/main.7c72efd2.chunk.js"
+  },
+  {
+    "revision": "d5ab20ffab2e5c7099ee",
+    "url": "/mern-My-Portfolio/static/js/runtime-main.99ca0c8d.js"
+  },
+  {
+    "revision": "fb26fc21a5d811d783ccff9d16c7a346",
+    "url": "/mern-My-Portfolio/static/media/2279565.fb26fc21.jpg"
+  },
+  {
+    "revision": "3aa9963a3dd5d695a70c53b4f86ebd6d",
+    "url": "/mern-My-Portfolio/static/media/Eric_Simmons_Resume.3aa9963a.pdf"
+  },
+  {
+    "revision": "20e1565dd5c9e47bd241d99c0dd55ad1",
+    "url": "/mern-My-Portfolio/static/media/PlayfairDisplay-VariableFont_wght.20e1565d.ttf"
+  },
+  {
+    "revision": "ab29f159197747e624ccffc601f541df",
+    "url": "/mern-My-Portfolio/static/media/e2.0.ab29f159.png"
+  },
+  {
+    "revision": "f4c84802117ce1d66e4d1535febaefff",
+    "url": "/mern-My-Portfolio/static/media/e3.5.f4c84802.png"
+  },
+  {
+    "revision": "bc1b0d816f932f73a8e5f2cd5861e8e7",
+    "url": "/mern-My-Portfolio/static/media/mern.bc1b0d81.png"
+  },
+  {
+    "revision": "c937fa1cfb71833e15625c047c9c045f",
+    "url": "/mern-My-Portfolio/static/media/pix2.c937fa1c.jpeg"
+  },
+  {
+    "revision": "60c8d5d25501391e792d06584e051051",
+    "url": "/mern-My-Portfolio/static/media/project1.60c8d5d2.jpg"
+  },
+  {
+    "revision": "aa692b8029b9a396dd3d1ffd925c4df2",
+    "url": "/mern-My-Portfolio/static/media/project2.aa692b80.jpg"
+  },
+  {
+    "revision": "004bf4388ae4451346fbf835853761a7",
+    "url": "/mern-My-Portfolio/static/media/project3.004bf438.png"
+  },
+  {
+    "revision": "8b7ae374c842a31f7cfea4656111e04f",
+    "url": "/mern-My-Portfolio/static/media/project4.8b7ae374.png"
+  },
+  {
+    "revision": "d730b6e245bc9edea057c529bec95f20",
+    "url": "/mern-My-Portfolio/static/media/project5.d730b6e2.png"
+  },
+  {
+    "revision": "0111b6fbcd441e346b632de63e3cc11b",
+    "url": "/mern-My-Portfolio/static/media/underc.0111b6fb.jpg"
+  }
+]);
